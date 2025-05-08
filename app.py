fb29f8ac68fc8694eda5333c06e1a8706a5209c0
rahulhirur <77510870+rahulhirur@users.noreply.github.com>
2025-05-08 20:07:45 +0200

    Update app.py
    added select box and confirmation button

--- app.py
+++ app.py
@@ -3,19 +3,31 @@ import streamlit as st
 from utils.read_ply import read_ply
 from utils.ply_to_fig import ply_to_fig
 
-st.set_page_config(page_title="3D Viewer", layout="wide")
+st.set_page_config(page_title="3D Viewer", layout="wide", initial_sidebar_state = "expanded")
 
 st.sidebar.title("3D Viewer - PLY File Reader")
 
 uploaded_file = st.sidebar.file_uploader("Upload a PLY file", type=["ply"])
+colorscales = [
+    "Viridis", "Cividis", "Inferno", "Magma", "Plasma", "Turbo", "Jet",
+    "Bluered", "RdBu", "Picnic", "Portland", "Blackbody", "Earth",
+    "Electric", "Hot", "Rainbow", "YlGnBu", "Greens", "YlOrRd", "Agsunset"
+]
 
-if uploaded_file is not None:
-    st.sidebar.success("File uploaded successfully!")
-    points = read_ply(uploaded_file)
-
-    if points is not None:
-        fig = ply_to_fig(points)
-        if fig is not None:
-            st.plotly_chart(fig, use_container_width=True)
-        else:
-            st.error("Failed to create figure from PLY data.")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+# Streamlit selectbox for choosing a colorscale
+selected_colorscale = st.selectbox("Choose a colorscale:", colorscales)
+if st.sidebar.button('Visualize'):
+    
+    if uploaded_file is not None:
+        if selected_colorscale is None:
+            selected_colorscale = 'viridis'
+        
+        st.sidebar.success("File uploaded successfully!")
+        points = read_ply(uploaded_file)
+    
+        if points is not None:
+            fig = ply_to_fig(points, selected_colorscale)
+            if fig is not None:
+                st.plotly_chart(fig, use_container_width=True)
+            else:
+                st.error("Failed to create figure from PLY data.")
